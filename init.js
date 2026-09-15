0a73c45e34c5320669f65006bc8b29a282c21cf2
basyura <basyura@gmail.com>
2026-09-15 23:26:45 +0900

    delete log

--- init.js
+++ init.js
@@ -647,13 +647,6 @@ document.addEventListener(
     }
 
     queueMicrotask(() => {
-      const time = new Date().toLocaleTimeString("ja-JP", {
-        hour12: false,
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      });
-      // console.log("save @", time);
       inkdrop.commands.dispatch(document.body, "core:save-note");
     });
   },
